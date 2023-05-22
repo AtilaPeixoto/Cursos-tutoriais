933f3df9a16190049b73aedd0ae6ef0dfe79a025
AtilaPeixoto <atila.yuri.peixoto@gmail.com>
2023-05-22 12:45:01 -0300

    'admin'

--- cursos-tutoriais/Tutorial_Django/tutorial/polls/models.py
+++ cursos-tutoriais/Tutorial_Django/tutorial/polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone 
 import datetime
+from django.contrib import admin
 # Create your models here.
 
 class Question(models.Model):
@@ -14,6 +15,14 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
             
+    @admin.display(
+    boolean=True,
+    ordering="pub_date",
+    description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
         
     
 class Choice(models.Model):
